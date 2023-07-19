c9dc08d3df48b72f874166ab0f814942e8228f46
Khushi Jain <96049770+khushi1351@users.noreply.github.com>
2023-07-19 12:17:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ const ham = document.getElementById('ham');
 const cross = document.getElementById('cross');
 const body = document.querySelector('body');
 const hamburger = document.querySelector('.hamburger-menu');
+const header = document.querySelector('header');
+const container = document.querySelector('.container')
 ham.addEventListener('click',()=>{
-    // console.log('hello');
     ham.style = "display: none !important";
     cross.style = "display: block !important";
     cross.style.color = "white";
-    hamburger.style = "transform: translateX(0%)"
+    hamburger.style = "transform: translateX(5%)";
+    header.style.opacity = "0.2";
+    container.style.opacity = "0.2";
 
 })
 
 cross.addEventListener('click', ()=>{
-    console.log('bye');
     cross.style = "display: none !important";
     ham.style = "display: block !important";
     ham.style.color = "white";
-    body.style.opacity = '1';
+     header.style.opacity = "1";
+    container.style.opacity = "1";
     hamburger.style = "transform: translateX(130%)"
 })
